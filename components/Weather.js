c4c48ee06db699fbd1558ff23fa3615a2e4c9825
tualimmanee <osuzumiyao@gmail.com>
2020-10-02 02:00:54 +0700

    opacity

--- components/Weather.js
+++ components/Weather.js
@@ -57,7 +57,7 @@ const styles = StyleSheet.create({
         borderRadius: 200,
         width: '100%',
         height: 250,
-        opacity: 0.2,
+        opacity: 0.4,
         alignItems: 'center',
     },
     medium: {
